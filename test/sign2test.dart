3c4c98c0b0d4e4c5d8ab2a06ffaf8ed2f21f6467
Ole Martin Gjersvik <olem@olem.org>
2013-05-25 16:30:33 +0200

    Written test specs for query string.

---
+++ test/sign2test.dart
@@ -0,0 +1,28 @@
+library sign2test;
+
+import 'package:unittest/unittest.dart';
+import 'package:amazone_dart/amazone_dart.dart';
+
+
+void main(){
+  group('Sign2:', () {
+    test('The sign string must contain &Version=2009-03-31', () {
+      fail('Not Implmented');
+    });
+    test('The sign string must contain &SignatureVersion=2', () {
+      fail('Not Implmented');
+    });
+    test('The sign string must contain &SignatureMethod=HmacSHA256', () {
+      fail('Not Implmented');
+    });
+    test('The sign string must contain &AWSAccessKeyId=x were x is the passed accessKey', () {
+      fail('Not Implmented');
+    });
+    test('The sign string must contain Timestamp=x, were x is url ecoded UTC datetime. 0000-00-00T00:00:00', () {
+      fail('Not Implmented');
+    });
+    test('SignatureMethod=HmacSHA256', () {
+      fail('Not Implmented');
+    });
+  });
+}
