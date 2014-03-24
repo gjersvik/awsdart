de2e49f1353b48770ff8203415d74c623c254722
Ole Martin Gjersvik <olem@olem.org>
2014-03-24 15:25:30 +0100

    Removed sign tests. Need to rewrite.

--- test/src/sign.dart
+++ test/src/sign.dart
@@ -11,44 +11,4 @@ signTest() => group('Sign',(){
   tearDown((){
     sign = null;
   });
-  
-  test('The exsample from doc to canonical',(){
-    var uri = Uri.parse('https://elasticmapreduce.amazonaws.com?' + 
-        'Action=DescribeJobFlows&Version=2009-03-31' +
-        '&AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&SignatureVersion=2' +
-        '&SignatureMethod=HmacSHA256&Timestamp=2011-10-03T15%3A19%3A30');
-    var test = 'GET\n' + 
-        'elasticmapreduce.amazonaws.com\n' +
-        '/\n' + 
-        'AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Action=DescribeJobFlows' +
-        '&SignatureMethod=HmacSHA256&SignatureVersion=2' +
-        '&Timestamp=2011-10-03T15%3A19%3A30&Version=2009-03-31';
-    
-    expect(sign.canonical2('GET', uri), test);
-  });
-  
-  test('The exsample from doc calculateSignature',(){
-    var data = 'GET\n' + 
-        'elasticmapreduce.amazonaws.com\n' +
-        '/\n' + 
-        'AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Action=DescribeJobFlows' +
-        '&SignatureMethod=HmacSHA256&SignatureVersion=2' +
-        '&Timestamp=2011-10-03T15%3A19%3A30&Version=2009-03-31';
-    
-    expect(sign.toUrl(sign.sign(sign.secretKey, data)), 
-        'i91nKc4PWAt0JJIdXwz9HxZCJDdiy6cf%2FMj6vPxyYIs%3D');
-  });
-  
-  test('The exsample from doc full signature',(){
-    var req = new Request();
-    req.metode = 'GET';
-    req.uri = Uri.parse('https://elasticmapreduce.amazonaws.com?' + 
-        'Action=DescribeJobFlows&Version=2009-03-31');
-    var time = new DateTime.utc(2011,10,3,15,19,30);
-    
-    req = sign.sign2(req, time);
-    
-    expect(req.uri.queryParameters['Signature'], 
-        'i91nKc4PWAt0JJIdXwz9HxZCJDdiy6cf%2FMj6vPxyYIs%3D');
-  });
 });
