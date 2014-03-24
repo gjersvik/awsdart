e77c5866136e9aee1ff83d6667f5ccec44583e83
Ole Martin Gjersvik <olem@olem.org>
2014-03-24 11:05:39 +0100

    Pelimenary version canoical4

--- lib/src/sign.dart
+++ lib/src/sign.dart
@@ -33,8 +33,7 @@ class Sign{
     canon.writeln(uri.authority);
     
     // Add the URL-encoded version of each path segment of the URI
-    var path = uri.pathSegments.map(Uri.encodeComponent).join('/');
-    canon.writeln('/$path');
+    canon.writeln('/${uri.path}');
     
     //Add the query string components as UTF-8 characters which are URL encoded
     //and sorted using lexicographic byte ordering.
@@ -48,9 +47,53 @@ class Sign{
     return canon.toString();
   }
   
+  String canonical4(String metode, Uri uri, 
+                    Map<String,String> headers, List<int> payloadHash){
+    var canon = new StringBuffer();
+    
+    // 1. HTTPRequestMethod
+    canon.writeln(metode);
+    // 2. CanonicalURI
+    canon.writeln('/${uri.path}');
+    // 3. CanonicalQueryString
+    canon.writeln(canonicalQueryString(uri.queryParameters)); 
+    // 4. CanonicalHeaders
+    canon.writeln(canonicalHeaders(headers));
+    // 5. SignedHeaders
+    canon.writeln(signedHeaders(headers));
+    // 6. PayloadHash
+    canon.write(CryptoUtils.bytesToHex(payloadHash));
+    
+    return canon.toString();
+  }
+  
   String calculateSignature2(String canonical, String secretKey){
     var hmac = new HMAC(new SHA256(),UTF8.encode(secretKey));
     hmac.add(UTF8.encode(canonical));
     return Uri.encodeComponent(CryptoUtils.bytesToBase64(hmac.close()));
+  }
+  
+  String canonicalQueryString(Map<String,String> query){
+    var keys = query.keys.toList();
+    keys.sort(); 
+    return keys.map((String key){
+      return Uri.encodeComponent(key) +'='+ Uri.encodeComponent(query[key]);
+    }).join('&');
+  }
+  
+  String canonicalHeaders(Map<String,String> headers){
+    var canon = new Map.fromIterables(
+        headers.keys.map((s)=>s.toLowerCase()),
+        headers.values.map((s)=>s.trim()));
+    
+    var headersList = canon.keys.toList();
+    headersList.sort();
+    return headersList.map((key) => '$key:${canon[key]}').join('\n');
+  }
+  
+  String signedHeaders(Map<String,String> headers){
+    var headersList = headers.keys.map((s)=>s.toLowerCase()).toList();
+    headersList.sort();
+    return headersList.join(';');
   }
 }
