043a452d2cfa18c1f6be74664edfc0c75b3fea82
Ole Martin Gjersvik <olem@olem.org>
2014-04-18 10:35:00 +0200

    Better handeling of data.

--- lib/html.dart
+++ lib/html.dart
@@ -2,7 +2,6 @@ library awsdart_html;
 
 import 'dart:async';
 import 'dart:html';
-import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:awsdart/core.dart';
@@ -13,22 +12,24 @@ setupAwsHtml(){
 }
 
 Future<Response> htmlRequester(Request req) {
+  var sendData = req.body;
+  if(req.method == 'GET' || req.body.isEmpty){
+    sendData = null;
+  } else {
+    var data = new Uint8List.fromList(req.body);
+    sendData = data.buffer;
+  }
+  
   return HttpRequest.request(req.uri.toString(), method: req.method,
-      requestHeaders: req.headers, sendData: req.body)
+      requestHeaders: req.headers, sendData: sendData,
+      responseType:'arraybuffer')
       .then((HttpRequest response){
         var res = new Response();
         res.statusCode =  response.status;
         res.statusString = response.statusText;
         res.headers = response.responseHeaders;
         var data = response.response;
-        if(data is Document){
-          data = data.toString();
-        }
-        if(data is String){
-          data = UTF8.encode(data);
-        }else if(data is Blob){
-          throw 'Dont know how to do Blob yet.';
-        }else if(data is ByteBuffer){
+        if(data is ByteBuffer){
           data = new Uint8List.view(data).toList();
         }else{
           data = [];
