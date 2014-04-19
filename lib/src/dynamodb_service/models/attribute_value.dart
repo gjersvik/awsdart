806ab93dd7790223bd1757e646447150b0503982
Ole Martin Gjersvik <olem@olem.org>
2014-04-19 21:47:18 +0200

    AttributeValue definition

--- lib/src/dynamodb_service/models/attribute_value.dart
+++ lib/src/dynamodb_service/models/attribute_value.dart
@@ -2,4 +2,22 @@ part of awsdart_dynamodb_service;
 
 class AttributeValue{
   
+  AttributeValue([dynamic value]);
+  AttributeValue.fromJson(Map);
+  
+  var value;
+  
+  List<int> b;
+  Set<List<int>> bs;
+  String s;
+  Set<String> ss;
+  num n;
+  Set<num> ns;
+  
+  Map toJson(){
+    
+  }
+  
+  String _type;
+  var _value;
 }
