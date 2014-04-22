867bfc6c117bcc232cc81fe701d59965aade65a9
Ole Martin Gjersvik <olem@olem.org>
2014-04-22 10:28:34 +0200

    Maped out the GetItemResult class

--- lib/dynamodb_service.dart
+++ lib/dynamodb_service.dart
@@ -7,4 +7,6 @@ import 'package:awsdart/core.dart';
 
 part 'src/dynamodb_service/dynamodb_service.dart';
 part 'src/dynamodb_service/models/attribute_value.dart';
+part 'src/dynamodb_service/models/capacity.dart';
+part 'src/dynamodb_service/models/consumed_capacity.dart';
 part 'src/dynamodb_service/models/get_item_result.dart';
--- lib/src/dynamodb_service/models/get_item_result.dart
+++ lib/src/dynamodb_service/models/get_item_result.dart
@@ -1,5 +1,16 @@
 part of awsdart_dynamodb_service;
 
 class GetItemResult{
+  ConsumedCapacity consumedCapacity;
+  final Map<String,AttributeValue> item = {};
   
+  GetItemResult();
+  GetItemResult.formJson();
+  
+  toJson(){
+    var data = {};
+    if(item.isNotEmpty){
+      data['Item'] = item;
+    }
+  }
 }
