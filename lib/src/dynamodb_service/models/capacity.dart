72034d1cf9a4add9e81b4373f0878abd1a033b96
Ole Martin Gjersvik <olem@olem.org>
2014-04-22 10:34:38 +0200

    Implmented Capacity.

--- lib/src/dynamodb_service/models/capacity.dart
+++ lib/src/dynamodb_service/models/capacity.dart
@@ -1,5 +1,20 @@
 part of awsdart_dynamodb_service;
 
 class Capacity{
+  double capacityUnits = 0.0;
   
+  Capacity();
+  Capacity.fromJson(Map json){
+    if(json.containsKey('CapacityUnits')){
+      capacityUnits = json['CapacityUnits'].toDouble();
+    }
+  }
+  
+  toJson(){
+    if(capacityUnits != 0.0){
+      return {'CapacityUnits': capacityUnits};
+    }else{
+      return {};
+    }
+  }
 }
